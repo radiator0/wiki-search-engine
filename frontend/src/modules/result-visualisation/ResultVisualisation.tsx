8804409ad2e025e1cc632538502626fda5aab124
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-05-08 16:44:11 +0200

    Delete parsoid

--- frontend/src/modules/result-visualisation/ResultVisualisation.tsx
+++ frontend/src/modules/result-visualisation/ResultVisualisation.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 
 interface IMatchParams {
-	id: string;
+  id: string;
 }
 
-
-interface IResultVisualisationProps extends RouteComponentProps<IMatchParams> {
-}
+interface IResultVisualisationProps extends RouteComponentProps<IMatchParams> {}
 
 export default function ResultVisualisation(props: IResultVisualisationProps) {
-	var Promise = require('prfun');
-	var Parsoid = require('parsoid-jsapi');
-	var main = Promise.async(function* () {
-		var text = "I love wikitext!";
-		var pdoc = yield Parsoid.parse(text, { pdoc: true });
-		console.log(pdoc.document.outerHTML);
-	});
-	main().done();
-
-	return (
-		<React.Fragment>
-		</React.Fragment>
-	);
+  return <React.Fragment></React.Fragment>;
 }
