b407d0f930c7df265435f280159ff2709c1a9d3f
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-05-10 20:39:35 +0200

    Change resultelement

--- frontend/src/modules/results/ResultElement.tsx
+++ frontend/src/modules/results/ResultElement.tsx
@@ -1,49 +1,46 @@
-import React from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import { IPage } from '../../models/page.model';
+import React from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import { IPage } from "../../models/page.model";
 
 const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			maxHeight: '13ch',
-			overflow: 'hidden',
-			backgroundColor: theme.palette.grey[50],
-			cursor: 'pointer',
-		},
-		inline: {
-			display: 'inline',
-		},
-	}),
+  createStyles({
+    root: {
+      maxHeight: "13ch",
+      overflow: "hidden",
+      backgroundColor: theme.palette.grey[50],
+    },
+    inline: {
+      display: "inline",
+    },
+  })
 );
 
 interface IResultElementProps extends IPage {
-	onClick: (number) => void
+  onClick: (number) => void;
 }
 
-export default function ResultElement(props : IResultElementProps) {
-	const classes = useStyles();
+export default function ResultElement(props: IResultElementProps) {
+  const classes = useStyles();
 
-	return (
-		<ListItem key={props.id} alignItems="flex-start" className={classes.root} onClick={() => props.onClick(props.id)}>
-			<ListItemText
-				primary={props.title}
-				secondary={
-					<React.Fragment>
-						<Typography
-							component="span"
-							variant="body2"
-							className={classes.inline}
-							color="textPrimary"
-						>
-							{props.id}
-          </Typography>
-						{props.content}
-					</React.Fragment>
-				}
-			/>
-		</ListItem>
-	);
+  return (
+    <ListItem key={props.id} alignItems="flex-start" className={classes.root}>
+      <ListItemText
+        primary={<a href={"./page/" + props.title}>{props.title}</a>}
+        secondary={
+          <React.Fragment>
+            <Typography
+              component="span"
+              variant="body2"
+              className={classes.inline}
+              color="textPrimary"
+            ></Typography>
+            {props.content}
+          </React.Fragment>
+        }
+      />
+    </ListItem>
+  );
 }
